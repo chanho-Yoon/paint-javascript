064306fad11b26a016d6a5e74f25932d87a1e9c3
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-12-31 23:22:29 +0900

    canvas.width height 추가
    canvas를 할 땐 해당영역 만큼의 width,와 height 를 줘야함

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
 const canvas = document.getElementById('jsCanvas')
 const ctx = canvas.getContext('2d')
+
 ctx.strokeStyle = '#2c2c2c'
 ctx.lineWidth = 2.5
 
+canvas.width = 700
+canvas.height = 700
 let painting = false
 function onMouseMove(event) {
   const x = event.offsetX
   const y = event.offsetY
-  if (!painting) {
+
+  if (painting === false) {
     ctx.beginPath()
     ctx.moveTo(x, y)
+    console.log(`true ${painting}`)
   } else {
     ctx.lineTo(x, y)
-    ctx.stroke(x, y)
+    ctx.stroke()
+    console.log(`false ${painting}`)
   }
 }
 function stopPainting() {
+  console.log('마우스 업')
   painting = false
 }
 function startPainting() {
+  console.log(' 마우스 다운 ')
   painting = true
 }
 function onMouseDown(event) {
