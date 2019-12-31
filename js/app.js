3a6084b4ebe853b532f56bf74bf64f8e77fcb42b
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-01 03:06:11 +0900

    img.png save 기능 구현
    canvas를 img로 만들기 위해서는
    link.href = toDataURL()이 필요
    그 뒤 가짜로 click을 넣어줄 a 태그를 생성
    link.download = '생성될 이름'
    link.click() // 링크 클릭한 것 처럼

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@ const ctx = canvas.getContext('2d')
 const colors = document.getElementsByClassName('jsColor')
 const range = document.getElementById('jsRange')
 const jsmode = document.getElementById('jsMode')
+const jssave = document.getElementById('jsSave')
 
 //FILL 눌렀을 시 해당되는 컬러를 적용하기 위한 변수
 let getColor = null
+
+//canvas 배경색을 처음에 초기화시켜주지 않아서 아무 색상없이 이미지 저장시 바탕화면이 투명하게 저장됨 이것을 초기화로 해결
+ctx.style = 'white'
+ctx.fillRect(0, 0, 700, 600)
 ctx.strokeStyle = '#2c2c2c'
 ctx.lineWidth = 2.5
 
@@ -39,23 +44,37 @@ function handleColorClick(event) {
   ctx.strokeStyle = getColor
   document.getElementById('jsChoiceColor').style.backgroundColor = getColor
 }
-function handleRange(event) {
-  getRangeValue = event.target.value
-  ctx.lineWidth = getRangeValue
+//오른쪽 마우스 클릭 막기
+function handleRightClick(event) {
+  event.preventDefault()
 }
-
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPainting)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
+  canvas.addEventListener('contextmenu', handleRightClick)
 }
 //controls__colors
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
 //range
+function handleRange(event) {
+  getRangeValue = event.target.value
+  ctx.lineWidth = getRangeValue
+}
 range.addEventListener('mouseup', handleRange)
 //FILL JSMODE
 function handleClickJsmode() {
-  canvas.style.backgroundColor = getColor
+  ctx.fillStyle = getColor
+  ctx.fillRect(0, 0, 700, 600)
 }
 jsmode.addEventListener('click', handleClickJsmode)
+//SAVE
+function handleClickJssave() {
+  const image = canvas.toDataURL('image/png')
+  const link = document.createElement('a')
+  link.href = image
+  link.download = 'happyIMG'
+  link.click()
+}
+jssave.addEventListener('click', handleClickJssave)
