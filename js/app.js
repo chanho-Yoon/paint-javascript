d582dc6c1accea92313849b9b6fff7d7705d94f8
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-01 01:09:21 +0900

    line width 변경하는 EventListener 작성
    슬라이더 중 마우스를 땠을 때 그 값이 되도록 하기 위해 'mouseup'

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('jsCanvas')
 const ctx = canvas.getContext('2d')
 const colors = document.getElementsByClassName('jsColor')
+const range = document.getElementById('jsRange')
 
 ctx.strokeStyle = '#2c2c2c'
 ctx.lineWidth = 2.5
@@ -34,10 +35,17 @@ function handleColorClick(event) {
   const getColor = event.target.style.backgroundColor
   ctx.strokeStyle = getColor
 }
+function handleRange(event) {
+  getRangeValue = event.target.value
+  ctx.lineWidth = getRangeValue
+}
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPainting)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
 }
+//controls__colors
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
+//range
+range.addEventListener('mouseup', handleRange)
