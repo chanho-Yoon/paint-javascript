cb1c757250eea5032c23d6d16284faaa0442d7a4
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-01-01 02:07:53 +0900

    FILL(채우기), choice color 기능 구현
    FILL 했을 시 현재 선택된 색상으로 canvas 바탕화면 채우기
    
    choice color은 controls__colors 에서 각각의 색상 선택시 선택한 색상을 표시해줌

--- js/app.js
+++ js/app.js
@@ -2,12 +2,15 @@ const canvas = document.getElementById('jsCanvas')
 const ctx = canvas.getContext('2d')
 const colors = document.getElementsByClassName('jsColor')
 const range = document.getElementById('jsRange')
+const jsmode = document.getElementById('jsMode')
 
+//FILL 눌렀을 시 해당되는 컬러를 적용하기 위한 변수
+let getColor = null
 ctx.strokeStyle = '#2c2c2c'
 ctx.lineWidth = 2.5
 
 canvas.width = 700
-canvas.height = 700
+canvas.height = 600
 let painting = false
 function onMouseMove(event) {
   const x = event.offsetX
@@ -32,13 +35,15 @@ function startPainting() {
 }
 
 function handleColorClick(event) {
-  const getColor = event.target.style.backgroundColor
+  getColor = event.target.style.backgroundColor
   ctx.strokeStyle = getColor
+  document.getElementById('jsChoiceColor').style.backgroundColor = getColor
 }
 function handleRange(event) {
   getRangeValue = event.target.value
   ctx.lineWidth = getRangeValue
 }
+
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPainting)
@@ -49,3 +54,8 @@ if (canvas) {
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
 //range
 range.addEventListener('mouseup', handleRange)
+//FILL JSMODE
+function handleClickJsmode() {
+  canvas.style.backgroundColor = getColor
+}
+jsmode.addEventListener('click', handleClickJsmode)
